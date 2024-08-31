4047b4ecf8b3b6dc3e6e01306c5a92db2e5fc8c9
dhanubor <nikhilmistry121203@gmail.com>
2024-08-31 20:44:41 +0600

    t

---
+++ letconst.js
@@ -0,0 +1,15 @@
+const babby = "Prionti Mondal"
+// babby = "priya"
+console.log(babby);
+
+
+
+const number = [12, 30]
+
+let sum = 0;
+for (let i = 0; i < i <10 ; i++) {
+    sum = sum +i;
+    
+    console.log(i);
+    
+}
